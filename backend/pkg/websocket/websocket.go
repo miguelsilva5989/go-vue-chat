939f420ef0acba082a6cdf32fe380372f77195ff
Mike <mike.5989@hotmail.com>
2020-06-26 15:02:20 +0200

    chat input and go websocket

---
+++ backend/pkg/websocket/websocket.go
@@ -0,0 +1,27 @@
+package websocket
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	// allow any connections
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// Upgrade allows a server application to call Upgrader.Upgrade method from an HTTP request handler to get a *Conn
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return conn, nil
+}
--- frontend/src/components/ChatArea.vue
+++ frontend/src/components/ChatArea.vue
@@ -1,52 +1,80 @@
 <template>
   <div class="chat">
-    <v-btn class="mb-3" small color="primary">Connect</v-btn>
+    <v-btn class="mb-3" small color="primary" @click="connectWs" :disabled="connected">Connect</v-btn>
 
-    <v-card elevation="16" max-width="600" class="mx-auto">
-      <v-virtual-scroll
-        :bench="benched"
-        :items="items"
-        max-height="800"
-        height="500"
-        item-height="64"
-      >
+    <v-card elevation="16" class="mx-auto">
+      <v-virtual-scroll :bench="benched" :items="items" height="510" item-height="64">
         <template v-slot="{ item }">
           <v-list-item :key="item">
             <v-list-item-action>
-              <v-btn fab small depressed color="primary">{{ user_id }}</v-btn>
+              <v-avatar color="primary" size="30" rounded>
+                <span class="white--text headline">{{ user_id }}</span>
+              </v-avatar>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>User Database Record</v-list-item-title>
+              <v-list-item-title>message...</v-list-item-title>
             </v-list-item-content>
 
             <v-list-item-action>
-              <v-icon small>mdi-trash-can</v-icon>
+              <v-icon>mdi-trash-can</v-icon>
             </v-list-item-action>
           </v-list-item>
 
           <v-divider></v-divider>
         </template>
       </v-virtual-scroll>
+
+      <v-text-field
+        v-model="messageInput"
+        filled
+        @keydown.enter="sendMsg"
+        :counter="maxLength"
+        placeholder="Write a message..."
+        :rules="[messageRules.required, messageRules.max]"
+      ></v-text-field>
     </v-card>
   </div>
 </template>
 
 <script>
+import ws from "../api";
+
 export default {
   name: "ChatArea",
   props: {
     user_id: Number
   },
   data: () => ({
-    benched: 1 // The number of items outside the user view that are rendered (even if they are not viewable); to help prevent empty white space when scrolling fast.
+    benched: 1, // The number of items outside the user view that are rendered (even if they are not viewable); to help prevent empty white space when scrolling fast
+    connected: false,
+    maxLength: 40,
+    messageInput: "",
+    messageRules: {
+      required: value => !!value || "Required.",
+      max: v => v.length <= 40 || "Max 40 characters"
+    }
   }),
+  methods: {
+    connectWs() {
+      ws.connect(msg => {
+        console.log(msg);
+      });
+      this.connected = !this.connected;
+    },
+    sendMsg() {
+      if (this.messageInput.length <= this.maxLength) {
+        console.log(this.messageInput);
+        this.messageInput = "";
+      }
+    }
+  },
   computed: {
     items() {
       return Array.from({ length: this.length }, (k, v) => v + 1);
     },
     length() {
-      return 7000;
+      return 30;
     }
   }
 };
